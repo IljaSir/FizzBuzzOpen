a5bfd057dc2abf22d207e8770542ddfab2e0a101
IljaSir <48324859+IljaSir@users.noreply.github.com>
2019-03-16 22:36:37 +0300

    Update Test.cpp

--- Test.cpp
+++ Test.cpp
@@ -1,6 +1,7 @@
+#include "FizzBuzz.h"
+/*
 #define BOOST_TEST_MODULE mytests
 #include <boost/test/included/unit_test.hpp>
-#include "FizzBuzz.h"
 
 BOOST_AUTO_TEST_SUITE(TestFizzBuzz)
 
@@ -22,3 +23,23 @@ BOOST_AUTO_TEST_CASE(Equal) {
 }
 
 BOOST_AUTO_TEST_SUITE_END()
+*/
+
+int main() {
+	ifstream input;
+	input.open("Tests.txt");
+	vector<string> test_str_vec;
+	FizzBuzz(input, test_str_vec);
+	input.close();
+	input.open("Answers.txt");
+	string ans;
+	int i = 0;
+	while (input >> ans)
+	{
+		assert(ans == test_str_vec[i]);
+		i++;
+	}
+	input.close();
+	cout << "Tests OK" << endl;
+	return 0;
+}
